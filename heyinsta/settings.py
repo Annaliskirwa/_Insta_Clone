0443abcd48d7735b967aca7fa7bb50f7eefd2509
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-06 21:34:49 +0300

    installed decouple

--- heyinsta/settings.py
+++ heyinsta/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
