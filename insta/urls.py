0bfd9d4ca9af02ff65dfb56058e33777de012d35
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-06 20:54:42 +0300

    Reconfiguring the paths

--- insta/urls.py
+++ insta/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
-from django.conf.urls import url
+# from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index,name = 'index'),
-    path('profile',views.show_profile, name='profile'),
-    # path('update_profile',views.update_profile, name='update_profile'),
-    path('posts/',views.new_post, name='post'),
-    path('search/', views.search, name='search'),
-    path('comment/<id>', views.comment, name='comment'),
-    path('update/<id>', views.update_profile, name='update_profile'),
-    path('signup/', views.signup, name='signup'),
+    path('',views.index, name='home'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow'),
+    path('like/<id>', views.like, name='like'),
+    path('post/<id>', views.comment, name='comment'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
 
 
 
