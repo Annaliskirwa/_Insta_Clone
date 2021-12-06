b763ae3c02ec7236fc3c1482b410e092601359bf
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-06 20:27:50 +0300

    Added a form for newsletters

--- insta/forms.py
+++ insta/forms.py
@@ -10,3 +10,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comments
         fields = ('comment',)
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+  
