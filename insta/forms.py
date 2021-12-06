14276cda951a9be087acbe52cb11966f245ff6e0
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-06 20:25:43 +0300

    Added form imports

---
+++ insta/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms.widgets import Textarea
+from .models import Image,Comments, Profile
