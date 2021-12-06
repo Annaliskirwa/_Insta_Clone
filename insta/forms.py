400a32498e790d6a909e9601b4d586c89c77b6b8
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-06 20:27:05 +0300

    Created a form for users to leave comments

--- insta/forms.py
+++ insta/forms.py
@@ -3,3 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.widgets import Textarea
 from .models import Image,Comments, Profile
+
+class CommentForm(forms.ModelForm):
+    comment = forms.CharField(label='Leave a comment',max_length=30)
+
+    class Meta:
+        model = Comments
+        fields = ('comment',)
