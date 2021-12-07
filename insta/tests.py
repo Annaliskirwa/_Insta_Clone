862a580a7f4d663547ccef173eb6112de74515f1
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-07 07:45:46 +0300

    Test class for Image model unit

--- insta/tests.py
+++ insta/tests.py
@@ -26,3 +26,33 @@ class ProfileTestCase(TestCase):
         self.profile.update_profile(self.profile.user_id)
         self.profile.save_profile()
         self.assertTrue(Profile,self.profile.user)
+
+
+class ImageTestClass(TestCase):
+    """
+    test class for Image model unit tests.
+    """
+    def setUp(self):
+        self.user = User.objects.create_user("username", "password")
+        self.new_profile = Profile(id = 12,profile_pic='profile.png',bio='this is a test profile',user=self.user)
+        self.new_profile.save()
+        self.newImage = Image(image='profile.png',caption="image", profile=self.new_profile)
+
+    def test_instance_true(self):
+        self.assertTrue(isinstance(self.newImage, Image))
+
+    def test_save_post(self):
+        self.newImage.save_post()
+        img = Image.objects.all()
+        self.assertTrue(len(img) == 1)
+
+    def test_delete_post(self):
+        self.newImage.save_post()
+        img = Profile.objects.all()
+        self.assertTrue(len(img) <= 1)
+
+    def test_get_image_by_id(self):
+        self.newImage.save_post()
+        img = self.newImage.get_image_by_id(self.newImage.id)
+        images = Image.objects.filter(id=self.newImage.id)
+        self.assertTrue(img, images)    
